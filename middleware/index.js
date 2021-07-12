9486d296a136b604c9f28a9ec6e5aa24bb12c9ae
Rahul Gowlapalli <63463686+rahulg009@users.noreply.github.com>
2021-07-12 21:52:53 +0530

    Delete index.js

--- middleware/index.js
+++
@@ -1,9 +0,0 @@
-const auth = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You need to be logged in to do that");
-    res.redirect("/login");
-}
-
-module.exports = auth;
