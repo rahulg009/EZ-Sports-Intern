e5f69cf4b46573eb7ec3e0d2a40c9468f755580f
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-07-12 18:13:32 +0530

    Admin routes added

---
+++ src/middleware/adminauth.js
@@ -0,0 +1,17 @@
+const Admin = require("../models/admin");
+const auth = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    Admin.findOne(req.user,
+      function (err, user) {
+        if (user) {
+          return next();
+        } else {
+          res.status(400).send("You are not an Admin");
+        }
+      });
+  } else {
+    res.status(400).send("Please login ");
+  }
+};
+
+module.exports = auth;
---
+++ src/models/admin.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+const adminSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email: {
+        type: String,
+        // required: true,
+        match: /.+\@.+\..+/,
+        trim: true,
+        lowercase: true,
+        unique: true
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+});
+
+adminSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("Admin", adminSchema);
