3d8c74f1386fe63fd0fbda2ef20eb731ef9177f8
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-08-12 22:10:18 +0530

    Update 2fa.js

--- src/routers/2fa.js
+++ src/routers/2fa.js
@@ -3,14 +3,14 @@ var nodemailer = require("nodemailer");
 
 const newSecret = twofactor.generateSecret({ name: "My Awesome App", account: "johndoe" });
 
-console.log(newSecret.secret)
+
 
 const newToken = twofactor.generateToken(newSecret.secret);
-console.log(newToken.token)
-// => { token: '630618' }
 
-console.log(twofactor.verifyToken(newSecret.secret, newToken.token));
-// => { delta: 0 }
 
-console.log(twofactor.verifyToken(newSecret.secret, "00"));
-// => null
+
+// console.log(twofactor.verifyToken(newSecret.secret, newToken.token));
+
+
+// console.log(twofactor.verifyToken(newSecret.secret, "00"));
+// // => null
